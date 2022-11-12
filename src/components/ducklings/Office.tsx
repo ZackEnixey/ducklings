7acc01037665fbfa9609f1eda6e775157200cd82
unknown <zoran.zilic.zack@gmail.com>
2022-11-12 18:03:15 +0100

    add cigarettes

--- src/components/ducklings/Office.tsx
+++ src/components/ducklings/Office.tsx
@@ -5,7 +5,7 @@ const Office = () => {
 
     return (
         <div>
-            <MenuBar /> jhf
+            <MenuBar />
             <div className="luka_trajector">
                 <div className="shaking_wrapper"></div>
             </div>
@@ -18,6 +18,38 @@ const Office = () => {
             <div className="minja_trajector">
                 <div className="shaking_wrapper"></div>
             </div>
+            <div className="nikola_trajector">
+                <div className="shaking_wrapper"></div>
+            </div>
+            
+            <div className="sara_trajector">
+                <div className="shaking_wrapper"></div>
+            </div>
+            <div className="pedja_trajector">
+                <div className="shaking_wrapper"></div>
+            </div>
+            <div className="srba_trajector">
+                <div className="shaking_wrapper"></div>
+            </div>
+
+            <div className="show_hide">
+                <div className="swinging" style={{top: "43vh", right: "0vw"}}>
+                    <div className="cigarette"></div>
+                </div>
+            </div>
+
+            <div className="show_hide">
+                <div className="swinging" style={{top: "51vh"}}>
+                    <div className="cigarette"></div>
+                </div>
+            </div>
+
+            <div className="show_hide">
+                <div className="swinging" style={{top: "60vh", right: "1vw"}}>
+                    <div className="cigarette"></div>
+                </div>
+            </div>
+            
         </div>
     )
 }
